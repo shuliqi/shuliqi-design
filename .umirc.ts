f4cfb77e39b2cac40f6de7add561efbba3baa698
shuliqi <shuliqi@bytedance.com>
2021-12-10 20:18:50 +0800

    feat：dumi配置

--- .umirc.ts
+++ .umirc.ts
@@ -6,4 +6,5 @@ export default defineConfig({
   logo: 'http://p6.qhimg.com/t01f5a5270b7946b505.jpg',
   outputPath: 'docs-dist',
   // more config: https://d.umijs.org/config
+  publicPath: "/shuliqi-design/"
 });
